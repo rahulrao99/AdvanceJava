00401a9b167028456c2a4d956004d67b350c94e1
Rahul Rao <131229520+rahulrao99@users.noreply.github.com>
2024-02-05 23:46:47 +0530

    Delete src directory

--- src/main/java/com/app/exception/EmployeeException.java
+++
@@ -1,8 +0,0 @@
-package com.app.exception;
-
-@SuppressWarnings("serial")
-public class EmployeeException extends RuntimeException{
-	public EmployeeException(String msg) {
-		super(msg);
-	}
-}
--- src/main/java/com/app/repository/EmpRepo.java
+++
@@ -1,8 +0,0 @@
-package com.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.app.entities.Employee;
-
-public interface EmpRepo extends JpaRepository<Employee, Long>{
-
-}
